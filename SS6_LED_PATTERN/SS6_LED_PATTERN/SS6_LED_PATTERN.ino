c0bdc6c8dd399e772d29b81d5f0384df510ce1c1
zenyj <jasonchang159@gmail.com>
2021-11-10 10:16:23 -0500

    .

--- SS6_LED_PATTERN/SS6_LED_PATTERN/SS6_LED_PATTERN.ino
+++ SS6_LED_PATTERN/SS6_LED_PATTERN/SS6_LED_PATTERN.ino
@@ -12,7 +12,6 @@ const int buttonPin = 3; // create a variable called buttonPin
 int buttonState = 0; // create a variable to track the button state and assign it to variable 0
 
 void setup() {
-  // put your setup code here, to run once:
  pinMode(LED1, OUTPUT); //initialize pins as outputs
  pinMode(LED2, OUTPUT);
  pinMode(LED3, OUTPUT);
@@ -23,36 +22,38 @@ void setup() {
  
 }
 void loop() {
-  // put your main code here, to run repeatedly:
- buttonState = digitalRead(buttonPin);
-delay(10); // small delay for debouncing
+buttonState = digitalRead(buttonPin);
+delay(10);
+ 
 // conditional statement
 
    if(buttonState == HIGH){ // if button is pressed
 digitalWrite(LED1, HIGH);// send voltage to LED Pin
 delay(1000);
-digitalWrite(LED2, HIGH);// send voltage to LED Pin
+digitalWrite(LED2, HIGH);
 delay(1000);
-digitalWrite(LED3, HIGH);// send voltage to LED Pin
+digitalWrite(LED3, HIGH);
 delay(1000);
-digitalWrite(LED4, HIGH);// send voltage to LED Pin
+digitalWrite(LED4, HIGH);
 delay(1000);
-digitalWrite(LED5, HIGH);// send voltage to LED Pin
+digitalWrite(LED5, HIGH);
 delay(1000);
-digitalWrite(LED6, HIGH);// send voltage to LED Pin
+digitalWrite(LED6, HIGH);
 delay(1000);
-}else {// otherwise
+
+}else {
+  
 digitalWrite(LED1, LOW); // turn off voltage to led pin
 delay(1000);
 digitalWrite(LED2, LOW);// send voltage to LED Pin
 delay(1000);
-digitalWrite(LED3, LOW);// send voltage to LED Pin
+digitalWrite(LED3, LOW);
 delay(1000);
-digitalWrite(LED4, LOW);// send voltage to LED Pin
+digitalWrite(LED4, LOW);
 delay(1000);
-digitalWrite(LED5, LOW);// send voltage to LED Pin
+digitalWrite(LED5, LOW);
 delay(1000);
-digitalWrite(LED6, LOW);// send voltage to LED Pin
+digitalWrite(LED6, LOW);
 delay(1000);
 }        
 }
